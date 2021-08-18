f1aa529bcf75b5e1592f602dab4793913a009d96
michiel <michelangelo.bernaerts@gmail.com>
2021-08-18 10:36:52 +0200

    restore

--- ckanext/deadoralive/logic/auth/get.py
+++ ckanext/deadoralive/logic/auth/get.py
@@ -5,20 +5,20 @@ import ckanext.deadoralive.config as config
 
 def get_resources_to_check(context, data_dict):
     """Only the configured users can get the get_resources_to_check API."""
-    return dict(success=context.get("user") in config.authorized_users)
-    # return dict(success=False)
+    # return dict(success=context.get("user") in config.authorized_users)
+    return dict(success=False)
 
 
 @toolkit.auth_sysadmins_check
 def get(context, data_dict):
     """Anyone can get the broken link report for a resource."""
-    return dict(success=True)
+    return dict(success=False)
 
 
 @toolkit.auth_sysadmins_check
 def broken_links_by_organization(context, data_dict):
     """Anyone can see the broken_links_by_organization report."""
-    return dict(success=True)
+    return dict(success=False)
 
 
 def broken_links_by_email(context, data_dict):
