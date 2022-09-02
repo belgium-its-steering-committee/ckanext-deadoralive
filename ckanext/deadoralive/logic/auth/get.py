89ab9633cd15696b2777507860607be091976883
Felten Vanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-09-02 21:02:40 +0200

    added filtering auth to brokenlinks url

--- ckanext/deadoralive/logic/auth/get.py
+++ ckanext/deadoralive/logic/auth/get.py
@@ -8,18 +8,26 @@ def get_resources_to_check(context, data_dict):
     return dict(success=context.get("user") in config.authorized_users)
 
 
+
 @toolkit.auth_sysadmins_check
 def get(context, data_dict):
-    """Anyone can get the broken link report for a resource."""
-    return dict(success=True)
+   """Anyone can get the broken link report for a resource."""
+   return dict(success=True)
 
 
 @toolkit.auth_sysadmins_check
 def broken_links_by_organization(context, data_dict):
     """Anyone can see the broken_links_by_organization report."""
-    return dict(success=True)
+    #if context.get(}) is True:
+    user = context['auth_user_obj']
+    if(user.sysadmin):
+        return dict(success=True)
+    return toolkit.abort(401,_('Unauthorized to read'))
 
 
 def broken_links_by_email(context, data_dict):
     """Only sysadmins can see the broken_links_by_email report."""
-    return dict(success=False)
+    user = context['auth_user_obj']
+    if(user.sysadmin):
+        return dict(success=True)
+    return toolkit.abort(401,_('Unauthorized to read'))
