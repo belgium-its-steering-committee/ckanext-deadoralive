7832e36b772141d8fad44b4b0568c7ebbf96ead5
geoFeltenVanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-10-04 00:49:07 +0200

    unAuthorized page

--- ckanext/deadoralive/controllers.py
+++ ckanext/deadoralive/controllers.py
@@ -6,9 +6,11 @@ import ckan.plugins.toolkit as toolkit
 class BrokenLinksController(toolkit.BaseController):
 
     def broken_links_by_organization(self):
-
-        report = toolkit.get_action(
-            "ckanext_deadoralive_broken_links_by_organization")(data_dict={})
+        try:
+            report = toolkit.get_action(
+                "ckanext_deadoralive_broken_links_by_organization")(data_dict={})
+        except toolkit.NotAuthorized:
+            toolkit.abort(401)
         extra_vars = {"organizations": report}
 
         return toolkit.render("broken_links_by_organization.html",
