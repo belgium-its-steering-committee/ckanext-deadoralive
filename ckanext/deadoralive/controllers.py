1db9c039cefcf077f2be0f3ac59010a7e3ad4ee0
geoFeltenVanballenberghe <felten.vanballenberghe@geosolutions.be>
2022-10-12 11:56:14 +0200

    vistors test

--- ckanext/deadoralive/controllers.py
+++ ckanext/deadoralive/controllers.py
@@ -11,8 +11,8 @@ class BrokenLinksController(toolkit.BaseController):
                 "ckanext_deadoralive_broken_links_by_organization")(data_dict={})
         except toolkit.NotAuthorized:
             toolkit.abort(401, "not authorized, please login")
+        
         extra_vars = {"organizations": report}
-
         return toolkit.render("broken_links_by_organization.html",
                               extra_vars=extra_vars)
 
--- ckanext/deadoralive/logic/auth/get.py
+++ ckanext/deadoralive/logic/auth/get.py
@@ -1,4 +1,5 @@
 """Authorization functions for the action functions in action/get.py."""
+from sre_constants import SUCCESS
 import ckan.plugins.toolkit as toolkit
 import ckanext.deadoralive.config as config
 
@@ -17,12 +18,12 @@ def get(context, data_dict):
 
 @toolkit.auth_sysadmins_check
 def broken_links_by_organization(context, data_dict):
-    """Anyone can see the broken_links_by_organization report."""
+    """Only sysadmins can see the broken_links_by_organization report."""
     #if context.get(}) is True:
     user = context['auth_user_obj']
     if(user.sysadmin):
         return dict(success=True)
-    return toolkit.abort(401,_('Unauthorized to read'))
+    return dict(success=False)
 
 
 def broken_links_by_email(context, data_dict):
@@ -30,4 +31,4 @@ def broken_links_by_email(context, data_dict):
     user = context['auth_user_obj']
     if(user.sysadmin):
         return dict(success=True)
-    return toolkit.abort(401,_('Unauthorized to read'))
+    return dict(succes=False)
